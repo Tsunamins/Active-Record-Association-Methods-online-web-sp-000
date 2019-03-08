20f3de94359f639647c69e936cb974a2c92d0a99
Alison Reilly <reillyamr@gmail.com>
2019-03-08 21:58:10 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.select(:name AS field_one")
+    self.artists.select('name AS field_one')
     
     
     
